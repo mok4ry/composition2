2de7ad9772a39d8dfc80bb7425c69f0641a36a49
Matt Mokary <mattmokary@gmail.com>
2018-10-29 22:43:24 -0400

    add blue block to color block tests

--- test/color_block.dart
+++ test/color_block.dart
@@ -6,10 +6,16 @@ import 'package:composition2/src/point.dart';
 import 'package:composition2/src/direction_pointer.dart';
 import 'package:composition2/src/codel_chooser.dart';
 
-final ColorBlock block_3x3 = ColorBlock([
+final ColorBlock block_3x3_white = ColorBlock([
   Codel(Colors.WHITE)..setPosition(Point(0, 0)), Codel(Colors.WHITE)..setPosition(Point(1, 0)), Codel(Colors.WHITE)..setPosition(Point(2, 0)),
   Codel(Colors.WHITE)..setPosition(Point(0, 1)), Codel(Colors.WHITE)..setPosition(Point(1, 1)), Codel(Colors.WHITE)..setPosition(Point(2, 1)),
   Codel(Colors.WHITE)..setPosition(Point(0, 2)), Codel(Colors.WHITE)..setPosition(Point(1, 2)), Codel(Colors.WHITE)..setPosition(Point(2, 2))
+]);
+
+final ColorBlock block_3x3_blue = ColorBlock([
+  Codel(Colors.BLUE)..setPosition(Point(0, 0)), Codel(Colors.BLUE)..setPosition(Point(1, 0)), Codel(Colors.BLUE)..setPosition(Point(2, 0)),
+  Codel(Colors.BLUE)..setPosition(Point(0, 1)), Codel(Colors.BLUE)..setPosition(Point(1, 1)), Codel(Colors.BLUE)..setPosition(Point(2, 1)),
+  Codel(Colors.BLUE)..setPosition(Point(0, 2)), Codel(Colors.BLUE)..setPosition(Point(1, 2)), Codel(Colors.BLUE)..setPosition(Point(2, 2))
 ]);
 
 void size_tests() {
@@ -21,55 +27,55 @@ void size_tests() {
   });
 
   test('size 3x3', () {
-    expect(block_3x3.size(), equals(9));
+    expect(block_3x3_white.size(), equals(9));
   });
 }
 
 void getExitBlock_tests() {
   test('dp up, cc left', () {
-    Codel exitBlock = block_3x3.getExitBlock(DirectionPointer.up, CodelChooser.left);
+    Codel exitBlock = block_3x3_blue.getExitBlock(DirectionPointer.up, CodelChooser.left);
 
     expect(exitBlock.getPosition(), equals(Point(0, 0)));
   });
 
   test('dp up, cc right', () {
-    Codel exitBlock = block_3x3.getExitBlock(DirectionPointer.up, CodelChooser.right);
+    Codel exitBlock = block_3x3_blue.getExitBlock(DirectionPointer.up, CodelChooser.right);
 
     expect(exitBlock.getPosition(), equals(Point(2, 0)));
   });
 
   test('dp right, cc left', () {
-    Codel exitBlock = block_3x3.getExitBlock(DirectionPointer.right, CodelChooser.left);
+    Codel exitBlock = block_3x3_blue.getExitBlock(DirectionPointer.right, CodelChooser.left);
 
     expect(exitBlock.getPosition(), equals(Point(2, 0)));
   });
 
   test('dp right, cc right', () {
-    Codel exitBlock = block_3x3.getExitBlock(DirectionPointer.right, CodelChooser.right);
+    Codel exitBlock = block_3x3_blue.getExitBlock(DirectionPointer.right, CodelChooser.right);
 
     expect(exitBlock.getPosition(), equals(Point(2, 2)));
   });
 
   test('dp down, cc left', () {
-    Codel exitBlock = block_3x3.getExitBlock(DirectionPointer.down, CodelChooser.left);
+    Codel exitBlock = block_3x3_blue.getExitBlock(DirectionPointer.down, CodelChooser.left);
 
     expect(exitBlock.getPosition(), equals(Point(2, 2)));
   });
 
   test('dp down, cc right', () {
-    Codel exitBlock = block_3x3.getExitBlock(DirectionPointer.down, CodelChooser.right);
+    Codel exitBlock = block_3x3_blue.getExitBlock(DirectionPointer.down, CodelChooser.right);
 
     expect(exitBlock.getPosition(), equals(Point(0, 2)));
   });
 
   test('dp left, cc left', () {
-    Codel exitBlock = block_3x3.getExitBlock(DirectionPointer.left, CodelChooser.left);
+    Codel exitBlock = block_3x3_blue.getExitBlock(DirectionPointer.left, CodelChooser.left);
 
     expect(exitBlock.getPosition(), equals(Point(0, 2)));
   });
 
   test('dp left, cc right', () {
-    Codel exitBlock = block_3x3.getExitBlock(DirectionPointer.left, CodelChooser.right);
+    Codel exitBlock = block_3x3_blue.getExitBlock(DirectionPointer.left, CodelChooser.right);
 
     expect(exitBlock.getPosition(), equals(Point(0, 0)));
   });
